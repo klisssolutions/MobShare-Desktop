7bd84bebe3ab901061475b19beef916d53d520c7
unknown <leonardp_cavalcante.07@hotmail.com>
2019-05-16 19:09:02 -0300

    first one

---
+++ Financeiro/src/main/Run.java
@@ -0,0 +1,14 @@
+package main;
+
+import view.FrmLogin;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		FrmLogin frmLogin = new FrmLogin();
+		frmLogin.setVisible(true);
+
+	}
+
+}
